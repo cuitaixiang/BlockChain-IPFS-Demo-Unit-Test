a748ae51c5ec9836988f8275ade89763033d997c
cuitaixiang <754060645@qq.com>
2020-04-24 22:16:16 +0800

    POTSimulation

--- src/test/java/io/taucoin/POTSimulation.java
+++ src/test/java/io/taucoin/POTSimulation.java
@@ -14,6 +14,9 @@ import static java.lang.Math.log;
 
 public class POTSimulation {
 
+    private final static int MAXRATIO = 335;
+    private final static int MINRATIO = 265;
+    private final static int AVERTIME = 300; //5 min
     private final static BigInteger DiffAdjustNumerator = new BigInteger("010000000000000000",16);
     private final static BigInteger DiffAdjustNumeratorHalf = new BigInteger("0100000000",16);
     private final static BigInteger DiffAdjustNumeratorCoe = new BigInteger("800000000000000",16); //2^59
@@ -23,19 +26,58 @@ public class POTSimulation {
     public static void main(String[] args) {
         byte[] genesisGenerationSignature = Hex.decode("442c29a4d18f192164006030640fb54c8b9ffd4f5750d2f6dca192dc653c52ad");
         BigInteger genesisBaseTarget = new BigInteger("0fffffffffffffff", 16);
-        BigInteger baseTarget = new BigInteger("369D0369D036978",16);
+        // base target    ave(s) max(s) 1000000 addresses
+        // 369D0369D036978   1  30/32
+        // 19D0369D036978    78 1044
+        // 12D0369D036978    108 1514
+        // 1FD0369D036978   63  866
+        // 20D0369D036978   61  857
+        // 21D0369D036978   60  819
+        BigInteger baseTarget = new BigInteger("21D0369D036978",16);
         byte[] generationSignature;
+        BigInteger hit;
+        BigInteger timeInterval;
         byte[] pubkey;
 
-        for (int i = 0; i < 3; i++) {
+        BigInteger maxTimeInterval = BigInteger.ZERO;
+        BigInteger totalTimeInterval = BigInteger.ZERO;
+
+        int N = 1000000;
+        for (int i = 0; i < N; i++) {
+            // get a public key
             ECKey ecKey = new ECKey(Utils.getRandom());
+            // calculate hit
             pubkey = ecKey.getPubKey();
-            byte[] data = new byte[genesisGenerationSignature.length + pubkey.length];
-            System.arraycopy(genesisGenerationSignature, 0, data, 0, genesisGenerationSignature.length);
-            System.arraycopy(pubkey, 0, data, genesisGenerationSignature.length, pubkey.length);
-            generationSignature = Sha256Hash.hash(data);
-            BigInteger hit = calculateRandomHit(generationSignature);
+            generationSignature = calculateNextBlockGenerationSignature(genesisGenerationSignature, pubkey);
+            hit = calculateRandomHit(generationSignature);
+            // calculate time interval = hit / base target
+            timeInterval = hit.divide(baseTarget);
+            logger.info("Time interval: {}", timeInterval);
+
+            totalTimeInterval = totalTimeInterval.add(timeInterval);
+            if (timeInterval.compareTo(maxTimeInterval) > 0 ) {
+                maxTimeInterval = timeInterval;
+            }
         }
+        logger.info("Base target:{}", baseTarget);
+        logger.info("Average time interval:{}", totalTimeInterval.divide(BigInteger.valueOf(N)));
+        logger.info("Max time interval:{}", maxTimeInterval);
+    }
+
+    /**
+     * get next block generation signature
+     *     Gn+1 = hash(Gn, pubkey)
+     * @param preGenerationSignature
+     * @param pubkey
+     * @return
+     */
+    public static byte[] calculateNextBlockGenerationSignature(byte[] preGenerationSignature, byte[] pubkey){
+        byte[] data = new byte[preGenerationSignature.length + pubkey.length];
+
+        System.arraycopy(preGenerationSignature, 0, data, 0, preGenerationSignature.length);
+        System.arraycopy(pubkey, 0, data, preGenerationSignature.length, pubkey.length);
+
+        return Sha256Hash.hash(data);
     }
 
     /**
@@ -48,20 +90,20 @@ public class POTSimulation {
         System.arraycopy(generationSignature,0,headBytes,0,8);
 
         BigInteger bhit = new BigInteger(1, headBytes);
-        logger.info("bhit:{}", bhit);
+//        logger.info("bhit:{}", bhit);
 
         BigInteger bhitUzero = bhit.add(BigInteger.ONE);
-        logger.info("bhitUzero:{}", bhitUzero);
+//        logger.info("bhitUzero:{}", bhitUzero);
 
         double logarithm = abs(log(bhitUzero.doubleValue()) - 2 * log(DiffAdjustNumeratorHalf.doubleValue()));
         logarithm = logarithm * 1000;
-        logger.info("logarithm:{}", logarithm);
+//        logger.info("logarithm:{}", logarithm);
 
         long ulogarithm = (new Double(logarithm)).longValue();
-        logger.info("ulogarithm:{}", ulogarithm);
+//        logger.info("ulogarithm:{}", ulogarithm);
 
         BigInteger adjustHit = DiffAdjustNumeratorCoe.multiply(BigInteger.valueOf(ulogarithm)).divide(BigInteger.valueOf(1000));
-        logger.info("adjustHit:{}", adjustHit);
+//        logger.info("adjustHit:{}", adjustHit);
 
         return adjustHit;
     }
